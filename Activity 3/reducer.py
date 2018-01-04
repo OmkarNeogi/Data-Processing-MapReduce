ec3c1a44213c88272476ee90a686fc7610ef44e3
Omkar Neogi <omkarneogi2@gmail.com>
2018-01-03 23:19:50 -0500

    Removed redundant commented code
    Had a two level if-else ladder of redundant code which was left behind by mistake.

--- Activity 3/reducer.py
+++ Activity 3/reducer.py
@@ -11,34 +11,6 @@ first_time = True
 for line in sys.stdin:
     line = line.strip()
     current_lemma, location = line.split("\t")
-    # print("sadf "+location)
-    #
-    # # if(current_lemma == old_lemma):
-    # #     print("here")
-    # #     locations_list_for_a_lemma.append(location)
-    # # else:
-    # #     if (first_time == True):
-    # #         first_time = False
-    # #         locations_list_for_a_lemma = []
-    # #         old_lemma = current_lemma
-    # #     else:
-    # #         print("%s\t%s" % (old_lemma, locations_list_for_a_lemma))
-    # #         locations_list_for_a_lemma = []
-    # #         locations_list_for_a_lemma.append(location)
-    # #         old_lemma = current_lemma
-    #
-    # if(current_lemma != old_lemma):
-    #     print("%s\t%s" % (current_lemma, locations_list_for_a_lemma))
-    #     locations_list_for_a_lemma = []
-    #     locations_list_for_a_lemma.append(location)
-    #     old_lemma = current_lemma
-    # else:
-    #     if(first_time == True):
-    #         first_time = False
-    #         locations_list_for_a_lemma.append(location)
-    #         old_lemma = current_lemma
-    #     else:
-    #         locations_list_for_a_lemma.append(location)
     if(first_time == True):
         first_time = False
         locations_list_for_a_lemma.append(location)
